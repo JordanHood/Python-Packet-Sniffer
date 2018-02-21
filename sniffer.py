48d53c485e6b64df4b5be596499dd381c2bcc1fd
Jordan Hood <jordan.hood@virginmedia.com>
2018-02-21 16:50:49 +0000

    filter udp traffic

--- sniffer.py
+++ sniffer.py
@@ -12,7 +12,6 @@ import sys
 def main(args):
     if args.interface:
         dev = args.interface
-        print('using args int')
     else: 
         #ask user to enter device name to sniff
         print ("Available devices are :")
@@ -22,7 +21,6 @@ def main(args):
     capture = pcapy.open_live(dev , 65536 , 1 , 0)
     if args.time:
         timeout = args.time
-        print('using args time')
     else:
         timeout = 30
     timeout_start = time.time()
@@ -43,7 +41,6 @@ def parse_packet(packet) :
     eth = unpack('!6s6sH' , eth_header)
     eth_protocol = socket.ntohs(eth[2])
     print ('Destination MAC : ' + eth_addr(packet[0:6]) + ', Source MAC : ' + eth_addr(packet[6:12]) + ', Protocol : ' + str(eth_protocol))
- 
     #Parse IP packets, IP Protocol number = 8
     if eth_protocol == 8 :
         ip_header = packet[eth_length:20+eth_length]
@@ -56,9 +53,75 @@ def parse_packet(packet) :
         protocol = iph[6]
         s_addr = socket.inet_ntoa(iph[8]);
         d_addr = socket.inet_ntoa(iph[9]);
-        print ('Version : ' + str(version) + ', IP Header Length : ' + str(ihl) + ', TTL : ' + str(ttl) + ', Protocol : ' + str(protocol) + ', Source Address : ' + str(s_addr) + ', Destination Address : ' + str(d_addr))
  
-        
+        print ('Version : ' + str(version) + ', IP Header Length : ' + str(ihl) + ', TTL : ' + str(ttl) + ', Protocol : ' + str(protocol) + ', Source Address : ' + str(s_addr) + ', Destination Address : ' + str(d_addr))
+        # #UDP packets
+        if protocol == 17 :
+            u = iph_length + eth_length
+            udph_length = 8
+            udp_header = packet[u:u+8]
+            print(udp_header)
+            udph = unpack('!HHHH' , udp_header)
+            source_port = udph[0]
+            dest_port = udph[1]
+            length = udph[2]
+            checksum = udph[3]
+            print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Length : ' + str(length) + ' Checksum : ' + str(checksum)
+            h_size = eth_length + iph_length + udph_length
+            data_size = len(packet) - h_size
+            data = packet[h_size:]
+            print 'Data : ' + data
+        # #TCP protocol
+        # if protocol == 6 :
+        #     t = iph_length + eth_length
+        #     tcp_header = packet[t:t+20]
+ 
+        #     tcph = unpack('!HHLLBBHHH' , tcp_header)
+             
+        #     source_port = tcph[0]
+        #     dest_port = tcph[1]
+        #     sequence = tcph[2]
+        #     acknowledgement = tcph[3]
+        #     doff_reserved = tcph[4]
+        #     tcph_length = doff_reserved >> 4
+             
+        #     print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length)
+             
+        #     h_size = eth_length + iph_length + tcph_length * 4
+        #     data_size = len(packet) - h_size
+             
+        #     #get data from the packet
+        #     data = packet[h_size:]
+             
+        #     print 'Data : ' + data
+ 
+        # #ICMP Packets
+        # elif protocol == 1 :
+        #     u = iph_length + eth_length
+        #     icmph_length = 4
+        #     icmp_header = packet[u:u+4]
+ 
+        #     icmph = unpack('!BBH' , icmp_header)
+             
+        #     icmp_type = icmph[0]
+        #     code = icmph[1]
+        #     checksum = icmph[2]
+             
+        #     print 'Type : ' + str(icmp_type) + ' Code : ' + str(code) + ' Checksum : ' + str(checksum)
+             
+        #     h_size = eth_length + iph_length + icmph_length
+        #     data_size = len(packet) - h_size
+             
+        #     #get data from the packet
+        #     data = packet[h_size:]
+             
+        #     print 'Data : ' + data 
+        # #some other IP packet like IGMP
+        # else :
+        #     print 'Protocol other than TCP/UDP/ICMP'
+             
+        # print
+ 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--time", help="The length of time to capture packets for", action="store",type=int, dest="time")
